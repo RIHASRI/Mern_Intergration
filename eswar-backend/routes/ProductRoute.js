2b70cc2559038255b590a22e35539e24ffb33aa0
RIHASRI <rihasri.js2024aids@sece.ac.in>
2025-12-18 11:59:02 +0530

    Update ProductRoute.js

--- eswar-backend/routes/ProductRoute.js
+++ eswar-backend/routes/ProductRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-const { getProduct, postProduct } = require("../controller/ProductController.JS"); 
-const { deleteProduct } = require("../controller/ProductController.JS");
-const { updateProduct } = require("../controller/ProductController.JS");
+const { getProduct, postProduct } = require("../controller/productController.js"); 
+const { deleteProduct } = require("../controller/productController.js");
+const { updateProduct } = require("../controller/productController.js");
 // MUST match file name exactly
 
 const router = express.Router();
