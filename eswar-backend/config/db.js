db6207b7337dadabc7e7b485e06a8578e69d3227
RIHASRI <rihasri.js2024aids@sece.ac.in>
2025-12-18 12:42:39 +0530

    Update db.js

--- eswar-backend/config/db.js
+++ eswar-backend/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-const connectdb = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/Eswarback");
-        console.log("Database Connected");   
-    } catch (error) {
-        console.error("mongodb connection err", error);
-        process.exit(1);   
-    }
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ Database Connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
-module.exports = connectdb;
+
+module.exports = connectDB;
